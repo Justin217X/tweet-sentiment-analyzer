15b3a4a6a39d1892cdaa89536a39f1bf093e392a
Justin217X <huedjustin@gmail.com>
2025-12-12 19:05:08 -0500

    removed old sentiment analyzer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import SentimentAnalyzer from "./components/SentimentAnalyzer";
 import TweetSentimentAnalyzer from "./components/TweetSentimentAnalyzer";
 
 function App() {
-  // return <SentimentAnalyzer />;
   return <TweetSentimentAnalyzer />;
 }
 
